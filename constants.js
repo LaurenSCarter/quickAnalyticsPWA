b272ccd1f77bcc2edb16f0155f49bc411789a114
Lauren <L.LaurenSCarter@gmail.com>
2026-01-06 08:37:03 +1100

    Update to match new deployment

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 // URL constants for Quick Analytics PWA
 const URLS = {
     // APPS Script Endpoint
-    LOG_ENTRY_ENDPOINT: 'https://script.google.com/macros/s/AKfycbwBw3cYQZqUBZZF7mLPH1s09EDN2a4FNTtZbfp_fZy0LBuThb0oi9pc5iM4BbkzqvFBxw/exec',
+    LOG_ENTRY_ENDPOINT: 'https://script.google.com/macros/s/AKfycbx5CxcsLraYpYSX6uq597Vx4i4PkFBd75UmAcwW3MIDzJu-K3rJ80OkbVDrT5QrOER59Q/exec',
     
     //Looker Studio Endpoints
     TODAYS_TIME_ENTRIES: 'https://lookerstudio.google.com/embed/reporting/6b9695d3-cc1f-4539-9b0f-b6918d6a360a/page/p_qxlby2dywd',
