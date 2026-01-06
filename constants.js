9a2b010df71641d22584c4387c9915270662253e
Lauren <L.LaurenSCarter@gmail.com>
2026-01-07 06:20:11 +1100

    refactor reporting pages so that we can add reports for logDoom and logPets (when they are ready)

--- constants.js
+++ constants.js
@@ -4,9 +4,18 @@ const URLS = {
     LOG_ENTRY_ENDPOINT: 'https://script.google.com/macros/s/AKfycbxhWGVWlfdnr_zL-LNYCU58p2Hx4lfYKMKpQqyco7efs-KAV5taJCb9gE1TuT-AiJaaFg/exec',
     
     //Looker Studio Endpoints
+    
+    // TIME REPORTS
     TODAYS_TIME_ENTRIES: 'https://lookerstudio.google.com/embed/reporting/6b9695d3-cc1f-4539-9b0f-b6918d6a360a/page/p_qxlby2dywd',
     WEEKLY_DASHBOARD: 'https://lookerstudio.google.com/embed/reporting/6b9695d3-cc1f-4539-9b0f-b6918d6a360a/page/mKhMF',
     WEEKLY_INSPIRATION: 'https://lookerstudio.google.com/embed/reporting/6b9695d3-cc1f-4539-9b0f-b6918d6a360a/page/p_mw7cs3plwd',
+
+    // DOOM SCROLL REPORTS
+    // TBA
+
+    // PET ANALYTIC REPORTS
+    // TBA
+
 };
 
 // Export for use in other modules
